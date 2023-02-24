5d170cc7fe9630b5d448622bda293b6fc54a1c94
dan delauro <dan.delauro@wexinc.com>
2023-02-24 06:45:05 -0500

    Add test bucket

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -4,3 +4,8 @@ resource "aws_s3_bucket" "sandbox_bucket" {
   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
 }
+
+resource "aws_s3_bucket_yolo" "sandbox_bucket_yolo" {
+  bucket = "aft-sandbox-yolo-${data.aws_caller_identity.current.account_id}"
+  acl    = "private"
+}
