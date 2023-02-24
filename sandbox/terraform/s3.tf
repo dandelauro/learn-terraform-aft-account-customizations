1a7bae08ea47ab56ce923c8592c9221c32e6b92c
dan delauro <dan.delauro@wexinc.com>
2023-02-24 06:57:03 -0500

    fixes s3 tf example

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -2,10 +2,8 @@ data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "sandbox_bucket" {
   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
-  acl    = "private"
 }
-
-resource "aws_s3_bucket_yolo" "sandbox_bucket_yolo" {
-  bucket = "aft-sandbox-yolo-${data.aws_caller_identity.current.account_id}"
+resource "aws_s3_bucket_acl" "sandbox_bucket_acl" {
+  bucket = aws_s3_bucket.sandbox_bucket.id
   acl    = "private"
 }
